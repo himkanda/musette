985b17234c185f1f55a7e5c6014f7dd42516d5c3
himkanda <136376478+himkanda@users.noreply.github.com>
2023-08-31 23:51:25 +0530

    updated databases name

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@ session_start();
 
 $timezone = date_default_timezone_set("Asia/Kolkata");
 
-$con = mysqli_connect("127.0.0.1", "root", "Test1234", "musette");
-$con2 = mysqli_connect("127.0.0.1", "root", "Test1234", "userdata");
+$con = mysqli_connect("127.0.0.1", "root", "Test1234", "musette_app_data");
+$con2 = mysqli_connect("127.0.0.1", "root", "Test1234", "musette_user_data");
 
 if (mysqli_connect_errno()) {
 	echo "Failed to connect: " . mysqli_connect_errno();
